c1e9575649a36f8758be8c1fe609e7569fbaddae
Maxsweel <maxsweelrodrigues@hotmail.com>
2021-03-18 19:38:42 -0300

    C02 - Organizamos em outros diretorios alguns arquivos e adicionamos o projeto aplicativoMsql.rar

---
+++ codeasp/outro/models/Contato.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAplication.Models
+{
+
+    /*
+    Nome da tabela para ser adicionada no banco / ser carregada do banco
+    */
+
+    [Table("Contato")]
+
+
+    public class Contato
+    {
+
+        //chamando coluna auto increment
+
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+
+        [Display (Name = "Email")]
+        [Column("Email")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Tel")]
+        [Column("Tel")]
+        public string Tel { get; set; }
+
+
+        [Display(Name = "Msg")]
+        [Column("Msg")]
+
+        public string Msg { get; set; }
+
+
+
+
+
+    }
+
+
+}
